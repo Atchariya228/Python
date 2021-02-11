f0c6595ad5c171ab64964bac9035098e67693c14
Atchariya Wiangkaew <atchariya.wi@kkumail.com>
2021-02-12 00:21:13 +0700

    upload

---
+++ Week3/test3_3.py
@@ -0,0 +1,16 @@
+#แบบฝึกหัด 3.3 Control Statements
+print('ป้อนชื่ออาหารสุดโปรดของคุณ หรือ exit เพื่อออกจากโปรแกรม')
+food = []
+i = 0
+while(True) :
+    i = i + 1
+    print('อาหารสุดโปรอันดับที่ ',i,end='')
+    choose = input(' คือ\t')
+    food.append(choose)
+    if( choose == 'exit') :
+        break
+print('อาหารสุดโปรดของคุณมีดังนี้',end=' ')
+for x in range(1,i) :
+    print(x,'.',food[x-1],end='    ')
+
+#Y
