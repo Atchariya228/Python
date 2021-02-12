2787b892e3d1f86812250a94aae92883bea3478a
Atchariya Wiangkaew <atchariya.wi@kkumail.com>
2021-02-12 15:54:31 +0700

    upload

--- test2_2.py
+++
@@ -1,9 +0,0 @@
-#แบบฝึกหัด 2.2
-h = 24
-m = 1440
-s = 3600
-print('Day converter Program')
-a = int(input('Input number of Days -->'))
-print(a,'Days --> Hour',a*h,'Hours')
-print(a,'Days --> Mintues',m*a,'Minutes')
-print(a,'Days --> Seconds',s*a,'Seconds')
